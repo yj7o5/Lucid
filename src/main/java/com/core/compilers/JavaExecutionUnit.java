db4ae1753ed18007bc235118aecf9e0dd34fb203
qmatejek <qmateak@yahoo.com>
2019-09-25 16:27:13 -0500

    This is my work in progress for the execution portion of the code.

--- src/main/java/com/core/compilers/JavaExecutionUnit.java
+++ src/main/java/com/core/compilers/JavaExecutionUnit.java
@@ -13,12 +13,23 @@ import java.util.stream.Stream;
 public class JavaExecutionUnit
 {
     private File workingDirectory;
+    private long execTime = 0;
 
     public JavaExecutionUnit(File directory)
     {
         workingDirectory = directory;
     }
 
+    //This should return the execution time of the Main.java if it is ran AFTER running the execute() method.
+    //Otherwise it will just return 0.
+    public long getExecTime()
+    {
+        return execTime;
+    }
+
+    //The project says that there will be a Main.java and I've found that as long as all the dependecies are compiled, the only
+    //one that actually needs to be executed is the Main. Which is why I just put the command together in this method instead
+    //of creating a method for that portion
     public String execute() throws Exception
     {
         if(workingDirectory == null || !workingDirectory.exists())
@@ -32,13 +43,40 @@ public class JavaExecutionUnit
         return executeCommand(command);
     }
 
-    //Not sure if the toString thing does what I think it does or not...
     public String executeCommand(String command) throws Exception
     {
-        Process proc = Runtime.getRuntime().exec(command);
+        StringBuilder result = new StringBuilder();
 
-        String result = proc.getInputStream().toString();
+        try {
+            Process proc = Runtime.getRuntime().exec(command);
 
-        return result;
+//        String result = proc.getInputStream().toString()
+
+            long start = System.nanoTime();
+            proc.waitFor();
+            long end = System.nanoTime();
+
+            execTime = (end-start) / 1_000_000; //gives run time of proc in milliseconds.
+
+            if (proc.waitFor() != 0)
+            {
+                result.append(String.format("Process Error - Exit Code : %s", proc.exitValue()));
+                //result.append(readProcessOutput(proc.getErrorStream()));
+
+                return result.toString();
+            }
+
+            result.append(proc.getInputStream());
+        }
+        catch(IOException e)
+        {
+            result.append(String.format("Error executing command: %s\nTrace: %s", command, e.getStackTrace()));
+        }
+        catch(Exception e)
+        {
+            result.append(String.format("Exception: %s\nStack: %s", e.getMessage(), e.getStackTrace()));
+        }
+
+        return result.toString();
     }
 }
